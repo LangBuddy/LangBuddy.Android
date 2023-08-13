1b287cd2b8c49a56425445ff7de9b19dc126d3fd
FremyXS <antonovch13@mail.ru>
2023-08-13 23:50:12 +0300

    refactor(layouts): add gradient

--- layouts/AuthLayout/AuthLayout.tsx
+++ layouts/AuthLayout/AuthLayout.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { View, Image } from "react-native";
 import { styles } from "./styles";
+import { LinearGradient } from 'expo-linear-gradient';
 
-export default function AuthLayout({children}:{children: React.ReactNode}) {
+export default function AuthLayout({ children }: { children: React.ReactNode }) {
     return (
-        <View style={styles.auth}>
+        <LinearGradient style={styles.auth}
+            colors={['#7309AA', 'rgba(161, 61, 213, 0.70)', 'rgba(175, 102, 213, 0.70)']}  >
             <Image style={styles.authImage}
                 source={require('../../assets/icons/icon.png')} />
             <View style={styles.authContainer}>
                 {children}
             </View>
-        </View>
+        </LinearGradient>
     )
 }
--- layouts/AuthLayout/styles.ts
+++ layouts/AuthLayout/styles.ts
@@ -3,7 +3,6 @@ import { StyleSheet } from 'react-native';
 export const styles = StyleSheet.create({
     auth: {
         flex: 1,
-        backgroundColor: 'linear-gradient(180deg, #7309AA 0%, rgba(161, 61, 213, 0.70) 51.04%, rgba(175, 102, 213, 0.70) 100%)',
         alignItems: 'center',
         paddingTop: 45,
     },
