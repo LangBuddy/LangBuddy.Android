1227e3ed88720f583d0e2607922b69240dafd218
FremyXS <antonovch13@mail.ru>
2023-08-13 17:37:39 +0300

    feat: add new icon
    User icon

--- components/Icon/index.ts
+++ components/Icon/index.ts
@@ -1,7 +1,9 @@
 import Email from "./Email";
 import Password from "./Password";
+import User from "./User";
 
 export const Icon = {
     Email: Email,
-    Password: Password
+    Password: Password,
+    User: User
 }
