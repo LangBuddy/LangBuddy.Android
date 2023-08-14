9367f4682f3b3b622c55990344ad672e78e9981c
FremyXS <antonovch13@mail.ru>
2023-08-14 12:26:32 +0300

    refactor: change button interface

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,16 +1,16 @@
 import React, { useState } from 'react';
-import { View, Text, TouchableOpacity, StyleProp, ViewStyle, GestureResponderEvent } from 'react-native';
-import { ButtonColorEnum, ButtonColorType } from './type';
+import { Text, TouchableOpacity, GestureResponderEvent } from 'react-native';
+import { ButtonColorEnum, ButtonColorType, ButtonStylesColorType } from './type';
 import { styles } from './style';
 
 interface IButton {
     title: string,
     onPress?: ((event: GestureResponderEvent) => void),
-    color: ButtonColorEnum
+    color: ButtonColorType
 }
 
 export default function Button({ title, color, onPress }: IButton) {
-    const [colorStyles, setColorStyles] = useState<ButtonColorType>();
+    const [colorStyles, setColorStyles] = useState<ButtonStylesColorType>();
 
     React.useEffect(() => {
         switch (color) {
--- components/Button/type.ts
+++ components/Button/type.ts
@@ -3,7 +3,14 @@ export enum ButtonColorEnum {
     linear = 'linear'
 }
 
-export type ButtonColorType = {
+export type ButtonColorType = 'linear' | 'solid';   
+
+export type ButtonStylesColorType = {
     colorButton: any,
     colorText: any,
+}
+
+export const ButtonColorType ={
+    solid: 'solid',
+    linear: 'linear',
 }
