db6b40474b5dbad7d9a02459397f12e07058426c
FremyXS <antonovch13@mail.ru>
2023-08-14 15:42:45 +0300

    refactor(Icon): add new exports

--- components/Icon/index.ts
+++ components/Icon/index.ts
@@ -1,7 +1,9 @@
+import Calendar from "./Calendar";
 import Email from "./Email";
+import Gender from "./Gender";
 import Password from "./Password";
 import User from "./User";
 
 export {
-    Email, Password, User
+    Email, Password, User, Calendar, Gender
 }
