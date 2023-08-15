e183b6b22ca9d9d2d666bfc8d10558411991ba42
FremyXS <antonovch13@mail.ru>
2023-08-15 14:04:59 +0300

    refactor(AuthLayout): change props
    Add title and subtitle

--- layouts/AuthLayout/styles.ts
+++ layouts/AuthLayout/styles.ts
@@ -11,6 +11,20 @@ export const styles = StyleSheet.create({
         width: 181,
         marginTop: 45,
     },
+    authTitle:{
+        fontSize: 32,
+        fontFamily: 'Montserrat-SemiBold',
+        fontWeight: '600',
+        color: '#fff',
+        marginTop: 43,
+    },
+    authSubtitle:{
+        color: '#FFF',
+        fontFamily: 'Montserrat-SemiBold',
+        fontSize: 16,
+        fontWeight: '600',
+        marginTop: 48
+    },
     authContainer: {
         flex: 1,
         maxWidth: 280,
---
+++ layouts/AuthLayout/types/index.d.ts
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export interface IAuthLayout {
+    children: React.ReactNode,
+    title?: string,
+    subtitle?: string,
+}
