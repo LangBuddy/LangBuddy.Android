16a6e6093a69bfc3ef98107f19537f279fc0e86e
FremyXS <antonovch13@mail.ru>
2023-09-12 21:14:21 +0300

    refactor: change file config-example

--- config-example.js
+++ config-example.js
@@ -1,6 +1,8 @@
+const AUTH_ENDPOINT = "*";
+
 export const AUTH_API = {
-    login: "",
-    register: "",
-    refresh: "",
-    logout: "",
+    login: "*",
+    register: "*",
+    refresh: "*",
+    logout: "*",
 }
