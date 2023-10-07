68e6116813b3d75571bdb54092402ecee3c5296e
fremyxs <antonovch13@mail.ru>
2023-10-07 12:01:56 +0500

    feat: update config

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,22 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+        ['module-resolver',
+          {
+            root: ['./'],
+            extensions: ['.ts', '.ios.ts', '.android.ts'],
+            alias: {
+                "components": "./components",
+                "hooks": "./hooks",
+                "layouts": "./layouts",
+                "navigators": "./navigators",
+                "screens": "./screens",
+                "types": "./types",
+                "utils": "./utils"
+            }
+          },
+        ],
+      ],
   };
 };
